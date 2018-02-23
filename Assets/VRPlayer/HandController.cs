8c3dd1ac1aeb7b552f6ab731795ecba5fb393d69
Brandon Canaday <brandon.canaday@yahoo.com>
2018-02-23 13:14:28 -0500

    added networking stuff from videos. need to extract what we need from this and learn how to do it ourselves

---
+++ Assets/VRPlayer/HandController.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class HandController : MonoBehaviour {
+
+	public VRPlayer player;
+	public Vector3 controllerVelocity;
+	public Vector3 controllerAngularVelocity;
+
+	public Transform attachedObject;
+	Transform attachPoint;
+	public bool attachExact = true;
+	public bool attachHide = false;
+	public float breakingDist = Mathf.Infinity;
+	private Collider lastIntersection;
+	public float maxSpeed = 5.0f; //don't let the object move too fast between frames
+
+	bool attachedKinematicSave;
+	float attachedMaxAngularVelSave;
+	Transform attachedParentSave;
+	public bool attachedUseGravitySave;
+	public float squeezeThreshold = .2f;
+
+	public bool teleporterActive = false;
+
+	public Transform teleporterBase; //set in the inspector, z forward is the telporter starting trajectory
+	public float teleporterArcSpeed = 10.0f;
+	public Vector3 teleporterArcGravity = new Vector3(0, -9.8f, 0);
+	public float arcMaxDistance = 100.0f;
+
+	public GameObject teleporterPointPrefab;
+	public GameObject teleporterHitVisualization; //set in the inspector
+	public List<GameObject> teleporterPoints = new List<GameObject>();  
+	// Use this for initialization
+	void Start () {
+		
+		attachPoint = (new GameObject()).transform;
+		attachPoint.SetParent(this.transform);
+		attachPoint.localPosition = Vector3.zero;
+		attachPoint.localRotation = Quaternion.identity;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		
+		if(attachedObject != null)
+		{
+			Rigidbody rb = attachedObject.GetComponent<Collider>().attachedRigidbody;
+			if (rb != null && !rb.isKinematic) 
+			{
+				Vector3 vel = ((attachPoint.position - attachedObject.position)) / Time.fixedDeltaTime;
+				if (vel.magnitude > maxSpeed)
+				{
+					vel = vel.normalized * maxSpeed;
+				}
+				rb.velocity = vel;
+
+				Quaternion rotDiff = attachPoint.rotation * Quaternion.Inverse(attachedObject.rotation);
+				float angle; Vector3 axis;
+				rotDiff.ToAngleAxis(out angle, out axis);
+				Vector3 angularVel = axis * angle * Mathf.Deg2Rad/Time.fixedDeltaTime;
+				rb.angularVelocity = angularVel;
+			}else
+			{ //just move it
+				attachedObject.position = attachPoint.position;
+				attachedObject.rotation = attachPoint.rotation;
+			}
+		}
+	}
+
+	public void grabObject(Transform other)
+	{
+		
+		if(attachedObject != null)
+		{
+			releaseObject();
+		}
+		attachedObject = other;
+		if (attachExact)
+		{
+			attachPoint.position = other.position;
+			attachPoint.rotation = other.rotation;
+		}else
+		{
+			attachPoint.position = this.transform.position;
+			attachPoint.rotation = other.rotation;
+		}
+		Rigidbody rb = other.GetComponent<Collider>().attachedRigidbody;
+		if (rb != null)
+		{
+			
+			attachedKinematicSave = rb.isKinematic;
+			attachedMaxAngularVelSave = rb.maxAngularVelocity;
+			attachedUseGravitySave = rb.useGravity;
+			rb.maxAngularVelocity = Mathf.Infinity;
+
+			
+		}
+		attachedParentSave = other.parent;
+	}
+
+	public void releaseObject()
+	{
+		attachedObject.SetParent(attachedParentSave);
+		if(attachedObject != null)
+		{
+			Rigidbody rb = attachedObject.GetComponent<Collider>().attachedRigidbody;
+			if (rb != null)
+			{
+				rb.isKinematic = attachedKinematicSave;
+				//rb.maxAngularVelocity = attachedMaxAngularVelSave;
+				
+				rb.velocity = controllerVelocity;
+				Vector3 between = attachedObject.position - transform.position;
+				rb.angularVelocity = controllerAngularVelocity;
+				rb.velocity = rb.GetRelativePointVelocity(between);
+			}
+		}
+		attachedObject = null;
+	}
+
+	
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.attachedRigidbody != null)
+		{
+			lastIntersection = other;
+		}
+	}
+	private void OnTriggerExit()
+	{
+		lastIntersection = null;
+	}
+	public void joystick(Vector2 direction)
+	{
+
+		float mag = direction.magnitude;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg -90;
+		//angle = 0 means fully pushed up, 90/-270 means pushed to the left
+
+		bool validLocation = false;
+		Vector3 hitPos = Vector3.zero;
+		Vector3 hitDir = Vector3.forward;
+		for(int i = 0; i < teleporterPoints.Count; i++)
+		{
+			teleporterPoints[i].SetActive(false);
+		}
+		teleporterHitVisualization.SetActive(false);
+		if (teleporterActive)
+		{
+			//compute arc points
+			float distanceTraveled = 0.0f;
+			Vector3 currentPos = teleporterBase.position;
+			Vector3 currentVel = teleporterBase.forward * teleporterArcSpeed;
+
+			int currentTeleporterPoint = 0;
+			
+
+			float dt = teleporterArcSpeed / 1000.0f;
+			while(distanceTraveled < arcMaxDistance)
+			{
+				
+
+				Vector3 nextPos = currentPos + currentVel * dt + .5f*teleporterArcGravity*dt*dt;
+				Vector3 nextVel = currentVel + teleporterArcGravity * dt;
+				Vector3 between = nextPos - currentPos;
+
+				//draw ray
+				if (teleporterPoints.Count <= currentTeleporterPoint)
+				{
+					//add  a point to the telporter
+					GameObject go = GameObject.Instantiate<GameObject>(teleporterPointPrefab);
+					go.transform.SetParent(this.transform);
+					teleporterPoints.Add(go);
+
+				}
+				teleporterPoints[currentTeleporterPoint].SetActive(true);
+				teleporterPoints[currentTeleporterPoint].transform.position = currentPos;
+				teleporterPoints[currentTeleporterPoint].transform.forward = between.normalized;
+
+				//perform raycast
+
+				RaycastHit hit;
+				if(Physics.Raycast(new Ray(currentPos, between.normalized),out hit, between.magnitude))
+				{
+					validLocation = true;
+					hitPos = hit.point;
+					hitDir = new Vector3(teleporterBase.forward.x, 0, teleporterBase.forward.z);
+					hitDir = Quaternion.AngleAxis(-angle, Vector3.up) * hitDir;
+					teleporterHitVisualization.SetActive(true);
+					teleporterHitVisualization.transform.position = hitPos;
+					teleporterHitVisualization.transform.forward = hitDir;
+					break;
+				}
+
+				currentPos = nextPos;
+				currentVel = nextVel;
+				distanceTraveled += between.magnitude;
+				currentTeleporterPoint++;
+			}
+		
+			
+		}
+
+
+		if(mag > .9f && !teleporterActive)
+		{
+			teleporterActive = true;
+		}else if(mag < .85f && teleporterActive && validLocation)
+		{
+			teleporterActive = false;
+			player.teleport(hitPos,hitDir);
+		}
+
+		
+	}
+
+	
+	public void squeeze(float ratio)
+	{
+
+		
+		if(ratio > squeezeThreshold)
+		{
+
+			if(attachedObject==null && lastIntersection != null)
+			{
+				grabObject(lastIntersection.transform);
+			}
+
+			if (attachedObject == null && lastIntersection == null)
+			{
+				Collider[] colliders = this.GetComponentsInChildren<Collider>();
+				foreach (Collider c in colliders)
+				{
+					c.isTrigger = false;
+				}
+			}
+
+		}
+		else
+		{
+			if(ratio <= squeezeThreshold / 2.0f)
+			{
+				if(attachedObject != null)
+				{
+					releaseObject();
+				}
+				
+				Collider[] colliders = this.GetComponentsInChildren<Collider>();
+				foreach (Collider c in colliders)
+				{
+					c.isTrigger = true;
+				}
+				
+			}
+
+
+		}
+	}
+
+
+}
---
+++ Assets/VRPlayer/VRPlayer.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.Networking;
+public class VRPlayer : NetworkBehaviour {
+	public Blinker hmdBlinker;
+	public Transform SteamVR_Rig;
+	public SteamVR_TrackedObject hmd;
+	public SteamVR_TrackedObject controllerLeft;
+	public SteamVR_TrackedObject controllerRight;
+	public Transform head;
+	public HandController handLeft;
+	public HandController handRight;
+	public Transform feet;
+	public enum LocomotionMode { TELEPORT, JOYSTICK_DRIVE, JOYSTICK_VEHICLE_DRIVE, FLYING, HUMAN_JOYSTICK, WALKING_IN_PLACE};
+	public LocomotionMode locomotionMode = LocomotionMode.JOYSTICK_DRIVE;
+	public Vector3 lastHipPosition;
+	public bool isWalking;
+	// Use this for initialization
+	[SyncVar]
+	Vector3 headPos;
+	[SyncVar]
+	Quaternion headRot;
+	[SyncVar]
+	Vector3 leftHandPos;
+	[SyncVar]
+	Quaternion leftHandRot;
+	[SyncVar]
+	Vector3 rightHandPos;
+	[SyncVar]
+	Quaternion rightHandRot;
+	void Start () {
+		head.transform.position = new Vector3(0, 2, 0);
+	}
+	
+	void Update () {
+	
+	}
+	private void FixedUpdate()
+	{
+		if (isLocalPlayer)
+		{
+			if (UnityEngine.XR.XRSettings.enabled)
+			{
+				if (SteamVR_Rig == null)
+				{
+					GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+					SteamVR_Rig = gm.vrCameraRig.transform;
+					hmd = gm.hmd;
+					controllerLeft = gm.controllerLeft;
+					controllerRight = gm.controllerRight;
+					hmdBlinker = gm.hmdBlinker;
+				}
+				//the controllers are the easy ones, just move them directly
+				copyTransform(controllerLeft.transform, handLeft.transform);
+				copyTransform(controllerRight.transform, handRight.transform);
+				//now move the head to the HMD position, this is actually the eye position
+				copyTransform(hmd.transform, head);
+				//move the feet to be in the tracking space, but on the ground (maybe do this with physics to ensure a good foot position later)
+				feet.position = Vector3.Scale(head.position, new Vector3(1, 0, 1)) + Vector3.Scale(SteamVR_Rig.position, new Vector3(0, 1, 0));
+				handleControllerInputs();
+
+			}
+			else
+			{
+				
+
+				float vertical = Input.GetAxis("Vertical");
+				float horizontal = Input.GetAxis("Horizontal");
+				transform.Translate(vertical * Time.fixedDeltaTime * (new Vector3(0, 0, 1)));
+				transform.Translate(horizontal * Time.fixedDeltaTime * (new Vector3(1, 0, 0)));
+
+
+			}
+			CmdSyncPlayer(head.transform.position,head.transform.rotation, handLeft.transform.position, handLeft.transform.rotation, handRight.transform.position, handRight.transform.rotation);
+		}else
+		{
+			//runs on all other clients and  the server
+			//move to the syncvars
+			head.position = Vector3.Lerp(head.position, headPos,.2f);
+			head.rotation = Quaternion.Slerp(head.rotation, headRot, .2f);
+			handLeft.transform.position = leftHandPos;
+			handLeft.transform.rotation = leftHandRot;
+			handRight.transform.position = rightHandPos;
+			handRight.transform.rotation = rightHandRot;
+
+		}
+
+	}
+	[Command]
+	void CmdSyncPlayer(Vector3 pos, Quaternion rot, Vector3 lhpos, Quaternion lhrot, Vector3 rhpos, Quaternion rhrot )
+	{
+		head.transform.position = pos;
+		head.transform.rotation = rot;
+		handLeft.transform.position = lhpos;
+		handRight.transform.position = rhpos;
+		handLeft.transform.rotation = lhrot;
+		handRight.transform.rotation = rhrot;
+		headPos = pos;
+		headRot = rot;
+		leftHandPos = lhpos;
+		leftHandRot = lhrot;
+		rightHandPos = rhpos;
+		rightHandRot = rhrot;
+
+	}
+	private void copyTransform(Transform from, Transform to)
+	{
+		to.position = from.position;
+		to.rotation = from.rotation;
+	}
+	private void handleControllerInputs()
+	{
+		int indexLeft = (int)controllerLeft.index;
+		int indexRight = (int)controllerRight.index;
+
+		handLeft.controllerVelocity = getControllerVelocity(controllerLeft);
+		handRight.controllerVelocity = getControllerVelocity(controllerRight);
+		handLeft.controllerAngularVelocity = getControllerAngularVelocity(controllerLeft);
+		handRight.controllerAngularVelocity = getControllerAngularVelocity(controllerRight);
+		
+		float triggerLeft = getTrigger(controllerLeft);
+		float triggerRight = getTrigger(controllerRight);
+
+		Vector2 joyLeft = getJoystick(controllerLeft);
+		Vector2 joyRight = getJoystick(controllerRight);
+		handLeft.squeeze(triggerLeft);
+		handRight.squeeze(triggerRight);
+
+		switch (locomotionMode)
+		{
+			case LocomotionMode.JOYSTICK_DRIVE:
+				{
+					drive(joyLeft, joyRight);
+					break;
+				}
+			case LocomotionMode.JOYSTICK_VEHICLE_DRIVE:
+				{
+					vehicleDrive(joyLeft, joyRight);
+					break;
+				}
+			case LocomotionMode.FLYING:
+				{
+					fly(joyLeft, joyRight);
+					break;
+				}
+			case LocomotionMode.HUMAN_JOYSTICK:
+				{
+					Vector3 footDispacement = (feet.position - SteamVR_Rig.position);
+					if (footDispacement.magnitude > .25f) {
+						vehicleDrive(new Vector2(footDispacement.x,footDispacement.z), Vector2.zero);
+					}
+					break;
+				}
+			case LocomotionMode.WALKING_IN_PLACE:
+				{
+					walkInPlace(joyLeft, joyRight);
+					break;
+				}
+			case LocomotionMode.TELEPORT:
+				{
+					handLeft.joystick(joyLeft);
+					handRight.joystick(joyRight);
+					break;
+				}
+		}
+		
+
+	}
+	private float getTrigger(SteamVR_TrackedObject controller)
+	{
+		return controller.index >= 0 ? SteamVR_Controller.Input((int)controller.index).GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).magnitude : 0.0f;
+	}
+
+	private Vector2 getJoystick(SteamVR_TrackedObject controller)
+	{
+		return controller.index >= 0 ? SteamVR_Controller.Input((int)controller.index).GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) : Vector2.zero;
+	}
+
+	private Vector3 getControllerVelocity(SteamVR_TrackedObject controller)
+	{
+		Vector3 controllerVelocity = controller.index >= 0 ? SteamVR_Controller.Input((int)controller.index).velocity : Vector3.zero;
+		return SteamVR_Rig.localToWorldMatrix.MultiplyVector(controllerVelocity.normalized)*controllerVelocity.magnitude;
+	}
+
+	private Vector3 getControllerAngularVelocity(SteamVR_TrackedObject controller)
+	{
+		Vector3 angularVelocity = controller.index >= 0 ? SteamVR_Controller.Input((int)controller.index).angularVelocity : Vector3.zero;
+		return SteamVR_Rig.localToWorldMatrix.MultiplyVector(angularVelocity.normalized) * angularVelocity.magnitude ;
+	}
+
+	public void teleport(Vector3 pos, Vector3 forward)
+	{
+		hmdBlinker.blink(.1f);
+		Vector3 offset = pos-feet.position;
+		SteamVR_Rig.Translate(offset, Space.World);
+		Vector3 facingDirection = new Vector3(head.forward.x,0,head.forward.z);
+		float angleBetween = Vector3.SignedAngle(facingDirection, forward, Vector3.up);
+		SteamVR_Rig.Rotate(Vector3.up, angleBetween, Space.World);
+	}
+	//called within an update method
+	public void drive(Vector2 leftJoystick, Vector2 rightJoystick)
+	{
+
+		//we'll use the gaze in the x-z plane as the facing direction
+		Vector3 facingDirection = new Vector3(head.forward.x, 0, head.forward.z);
+		Vector3 rightDirection = new Vector3(head.right.x, 0, head.right.z);
+		Vector3 displacement = (facingDirection * leftJoystick.y + rightDirection*leftJoystick.x) * Time.deltaTime;
+		SteamVR_Rig.transform.Translate(displacement, Space.World);
+		float angleDisplacement = 90*rightJoystick.x * Time.deltaTime;
+		SteamVR_Rig.transform.Rotate(0, angleDisplacement, 0, Space.World);
+
+	}
+
+	//called within an update method
+	public void vehicleDrive(Vector2 leftJoystick, Vector2 rightJoystick)
+	{
+
+		//we'll use the gaze in the x-z plane as the facing direction
+		Vector3 facingDirection = SteamVR_Rig.forward;
+		Vector3 rightDirection = SteamVR_Rig.right;
+		Vector3 displacement = (facingDirection * leftJoystick.y + rightDirection * leftJoystick.x) * Time.deltaTime;
+		SteamVR_Rig.transform.Translate(displacement, Space.World);
+		float angleDisplacement = 90 * rightJoystick.x * Time.deltaTime;
+		SteamVR_Rig.transform.Rotate(0, angleDisplacement, 0, Space.World);
+
+	}
+
+	public void fly(Vector2 leftJoystick, Vector2 rightJoystick)
+	{
+		float leftSpeed = Mathf.Clamp(leftJoystick.y,0,1);
+		float rightSpeed = Mathf.Clamp(rightJoystick.y, 0, 1);
+		Vector3 leftDirection = handLeft.transform.forward;
+		Vector3 rightDirection = handRight.transform.forward;
+		Vector3 displacement = (leftDirection * leftSpeed + rightDirection * rightSpeed) * Time.deltaTime;
+		SteamVR_Rig.transform.Translate(displacement, Space.World);
+	}
+
+	public void walkInPlace(Vector2 leftJoystick, Vector2 rightJoystick)
+	{
+		Vector3 rightVector = handRight.transform.position - handLeft.transform.position;
+		rightVector = new Vector3(rightVector.x, 0, rightVector.z);
+		Vector3 facingDirection = Vector3.Cross(rightVector.normalized, Vector3.up);
+		if(Mathf.Abs(leftJoystick.y) > 0.1f && Mathf.Abs(rightJoystick.y) > 0.1f )
+		{
+			Vector3 hipPosition = (handLeft.transform.position + handRight.transform.position) / 2.0f;
+			if (!isWalking)
+			{
+				isWalking = true;
+				lastHipPosition = hipPosition;
+			}
+			else
+			{
+				//figure out the y displacement of the central point
+				float yDisplacement = Mathf.Abs(((handLeft.transform.position + handRight.transform.position) / 2).y - lastHipPosition.y);
+				//go forward by that y displacement
+				SteamVR_Rig.Translate(yDisplacement * facingDirection, Space.World);
+			}
+		}
+		else
+		{
+			isWalking = false;
+		}
+
+	}
+
+
+
+}
