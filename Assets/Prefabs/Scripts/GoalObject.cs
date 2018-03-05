0e300eed057c8fa17a988471b0f78d0822dd8a15
Brandon Canaday <brandon.canaday@yahoo.com>
2018-03-04 22:30:56 -0500

    finished TaskObject functionality. just need to add some sort of static bowl object inside task space so that when rover brings goal objects back, it can place them inside the bowl so that they dont roll away again

---
+++ Assets/Prefabs/Scripts/GoalObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObject : MonoBehaviour {
+
+	private bool insideTaskSpace;
+
+	// Use this for initialization
+	void Start () {
+		insideTaskSpace = true;
+	}
+
+	// --------------------------------
+
+	public void setInsideTaskSpace(bool insideTaskSpace) {
+		this.insideTaskSpace = insideTaskSpace;
+	}
+
+	public bool getInsideTaskSpace() {
+		return this.insideTaskSpace;
+	}
+}
